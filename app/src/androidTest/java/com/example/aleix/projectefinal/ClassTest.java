7202992bf629fbf70c5139f37591fcf029e2afc4
aleixventura <aleixventura@hotmail.com>
2015-05-21 19:45:39 +0200

    Petit fix. Aleix Ajuntar tot

---
+++ app/src/androidTest/java/com/example/aleix/projectefinal/ClassTest.java
@@ -0,0 +1,14 @@
+package com.example.aleix.projectefinal;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by Aleix on 21/05/2015. ProjecteFinal
+ */
+public class ClassTest extends InstrumentationTestCase {
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 5;
+        assertEquals(expected, reality);
+    }
+}
